4b329acf3cddde36d9d437db733b20e2bdb03986
Ubuntu <azureuser@KYVM1main.txq5gch0hhgu5hevugqho5mvkf.bx.internal.cloudapp.net>
2021-12-15 12:36:02 +0000

    App working in container with nginx

--- application/__init__.py
+++ application/__init__.py
@@ -3,15 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:3306/books'
-
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@mysql:3306/books'
 app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
+db = SQLAlchemy(app)
 
 import application.routes
 import application.models
---
+++ application/tests/test_app.py
@@ -0,0 +1,25 @@
+from flask_testing import TestCase
+from application import app, db
+from application.models import Tasks
+from flask import url_for
+from datetime import date, timedelta
+
+class TestBase(TestCase):
+    def create_app(self):
+        app.config.update(SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:root@mysql:3306/books',
+            SECRET_KEY='test_secret_key',
+            DEBUG=True,
+            WTF_CSFR_ENABLED = False)
+        return app
+
+    def setUp(self):
+        db.create_all()
+        Auto_date = date.today
+        Default_target_date = date.today() + timedelta(days=60)
+        sample = Tasks(book_name = 'Test book', suthor = 'Test author', date_added = Auto_date, book_status = 'Proposed', date_target = Default_target_date, suggested_by='Tester')
+        db.session.add(sample)
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
