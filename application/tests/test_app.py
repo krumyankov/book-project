6a821335e4b98a7e1789f075b2b416115b5214e8
krumyankov <92857127+krumyankov@users.noreply.github.com>
2021-12-17 13:38:58 +0000

    Update test_app.py

--- application/tests/test_app.py
+++ application/tests/test_app.py
@@ -1,12 +1,12 @@
 from flask_testing import TestCase
 from application import app, db
-from application.models import Tasks
+from application.models import Books
 from flask import url_for
 from datetime import date, timedelta
 
 class TestBase(TestCase):
     def create_app(self):
-        app.config.update(SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:root@mysql:3306/books',
+        app.config.update(SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:root@localhost:3306/books',
             SECRET_KEY='test_secret_key',
             DEBUG=True,
             WTF_CSFR_ENABLED = False)
@@ -16,7 +16,7 @@ class TestBase(TestCase):
         db.create_all()
         Auto_date = date.today
         Default_target_date = date.today() + timedelta(days=60)
-        sample = Tasks(book_name = 'Test book', suthor = 'Test author', date_added = Auto_date, book_status = 'Proposed', date_target = Default_target_date, suggested_by='Tester')
+        sample = Books(book_name = 'Test book', suthor = 'Test author', date_added = Auto_date, book_status = 'Proposed', date_target = Default_target_date, suggested_by='Tester')
         db.session.add(sample)
         db.session.commit()
 
